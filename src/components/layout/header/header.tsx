094cfd4050d7bf30742f15db745531cc9f3f0fba
mesoflix <209336738+mesoflix@users.noreply.github.com>
2025-07-02 15:28:17 +0300

    Update header.tsx

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -80,7 +80,7 @@ const AppHeader = observer(() => {
                     <Button
                         tertiary
                         onClick={() => {
-                            window.location.replace('https://oauth.deriv.com/oauth2/authorize?app_id=69271&l=EN&brand=binaryfx');
+                            window.location.replace('https://oauth.deriv.com/oauth2/authorize?app_id=83090&l=EN&brand=mesoflixpaigy');
                         }}
                     >
                         <Localize i18n_default_text='Log in' />
